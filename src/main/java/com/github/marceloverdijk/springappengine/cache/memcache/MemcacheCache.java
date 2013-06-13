3b501a78b066ce2ca1a50c10515feacc4df6c6d6
Marcel Overdijk <marcel.overdijk@asml.com>
2013-06-13 12:12:32 +0200

    Updated license etc

---
+++ src/main/java/com/github/marceloverdijk/springappengine/cache/memcache/MemcacheCache.java
@@ -0,0 +1,39 @@
+package com.github.marceloverdijk.springappengine.cache.memcache;
+
+import org.springframework.cache.Cache;
+
+public class MemcacheCache implements Cache {
+
+    @Override
+    public void clear() {
+        // TODO
+    }
+
+    @Override
+    public void evict(Object key) {
+        // TODO
+    }
+
+    @Override
+    public ValueWrapper get(Object key) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Object getNativeCache() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+        // TODO
+    }
+}
--- src/main/java/com/github/marceloverdijk/springappengine/cache/memcache/MemcacheCacheManager.java
+++ src/main/java/com/github/marceloverdijk/springappengine/cache/memcache/MemcacheCacheManager.java
@@ -1,5 +1,15 @@
 package com.github.marceloverdijk.springappengine.cache.memcache;
 
-public class MemcacheCacheManager {
-    
+import java.util.Collection;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.transaction.AbstractTransactionSupportingCacheManager;
+
+public class MemcacheCacheManager extends AbstractTransactionSupportingCacheManager {
+
+    @Override
+    protected Collection<? extends Cache> loadCaches() {
+        // TODO
+        return null;
+    }
 }
