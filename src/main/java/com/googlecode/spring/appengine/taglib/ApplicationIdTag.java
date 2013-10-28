6088145792d319b8c32dd9fb8586fa3b2786721d
Marcel Overdijk <marcel@overdijk.me>
2013-10-28 22:04:34 +0100

    added gae taglib

---
+++ src/main/java/com/googlecode/spring/appengine/taglib/ApplicationIdTag.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.spring.appengine.taglib;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.springframework.web.util.TagUtils;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+/**
+ * JSP {@link Tag} which outputs the application identifier for the 
+ * current application. 
+ * Optionally exposes a <code>String</code> scripting variable containing the 
+ * application identifier. 
+ * 
+ * @author Marcel Overdijk
+ * @since 0.2
+ * @see SystemProperty#applicationId
+ */
+@SuppressWarnings("serial")
+public class ApplicationIdTag extends TagSupport {
+
+    private String var;
+    
+    private int scope = PageContext.PAGE_SCOPE;
+    
+    @Override
+    public int doEndTag() throws JspException {
+        String applicationId = SystemProperty.applicationId.get();
+        if (this.var == null) {
+            try {
+                pageContext.getOut().print(applicationId);
+            }
+            catch (IOException e) {
+                throw new JspException(e);
+            }
+        }
+        else {
+            pageContext.setAttribute(var, applicationId, scope);
+        }
+        return Tag.EVAL_PAGE;
+    }
+
+    /**
+     * Set the variable name to expose the application identifier under. 
+     * Defaults to rendering the application identifier to the current 
+     * {@link javax.servlet.jsp.JspWriter}.
+     */
+    public void setVar(String var) {
+        this.var = var;
+    }
+    
+    /**
+     * Set the scope to export the application identifier variable to. 
+     * This attribute has no meaning unless var is also defined.
+     * Defaults to {@link PageContext#PAGE_SCOPE}.
+     */
+    public void setScope(String scope) {
+        this.scope = TagUtils.getScope(scope);
+    }
+}
---
+++ src/main/java/com/googlecode/spring/appengine/taglib/DevelopmentTag.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.spring.appengine.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.springframework.web.util.TagUtils;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+/**
+ * Conditional JSP {@link Tag} which evaluates its body if the current executing 
+ * environment is <code>Development</code>. 
+ * Optionally exposes a <code>Boolean</code> scripting variable representing the 
+ * evaluation of <code>SystemProperty.environment.value() == SystemProperty.Environment.Value.Development</code>.
+ * 
+ * @author Marcel Overdijk
+ * @since 0.2
+ * @see SystemProperty#environment
+ */
+@SuppressWarnings("serial")
+public class DevelopmentTag extends TagSupport {
+
+    private String var;
+    
+    private int scope = PageContext.PAGE_SCOPE;
+
+    @Override
+    public int doStartTag() throws JspException {
+        boolean development = SystemProperty.environment.value() == SystemProperty.Environment.Value.Development;
+        if (var != null) {
+            pageContext.setAttribute(var, development, scope);
+        }
+        return development ? Tag.EVAL_BODY_INCLUDE : Tag.SKIP_BODY;
+    }
+
+    /**
+     * Set the variable name to expose the value under. 
+     */
+    public void setVar(String var) {
+        this.var = var;
+    }
+    
+    /**
+     * Set the scope to export the variable to. 
+     * This attribute has no meaning unless var is also defined.
+     * Defaults to {@link PageContext#PAGE_SCOPE}.
+     */
+    public void setScope(String scope) {
+        this.scope = TagUtils.getScope(scope);
+    }
+}
