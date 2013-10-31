6929888b6130c8548c79429962fb4120c5cb86bd
Marcel Overdijk <marcel@overdijk.me>
2013-10-31 21:12:43 +0100

    taglib junit tests

---
+++ src/test/java/com/googlecode/spring/appengine/taglib/LogoutUrlTagTests.java
@@ -0,0 +1,54 @@
+package com.googlecode.spring.appengine.taglib;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.servlet.jsp.PageContext;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Marcel Overdijk
+ */
+public class LogoutUrlTagTests extends AbstractTagTests {
+
+    private LogoutUrlTag tag;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        tag = new LogoutUrlTag();
+        tag.setPageContext(pageContext);
+        tag.setDestinationUrl("/my-destination-url");
+    }
+
+    @Test
+    public void testRendersLoginUrl() throws Exception {
+        tag.doStartTag();
+        tag.doEndTag();
+        String output = ((MockHttpServletResponse) pageContext.getResponse()).getContentAsString();
+        assertEquals("/_ah/logout?continue=%2Fmy-destination-url", output);
+    }
+
+    @Test
+    public void testVarDefaultScope() throws Exception {
+        tag.setVar("myVar");
+        tag.doStartTag();
+        tag.doEndTag();
+        String output = ((MockHttpServletResponse) pageContext.getResponse()).getContentAsString();
+        assertEquals("", output);
+        assertEquals("/_ah/logout?continue=%2Fmy-destination-url", pageContext.getAttribute("myVar", PageContext.PAGE_SCOPE));
+    }
+
+    @Test
+    public void testVarExplicitScope() throws Exception {
+        tag.setVar("myVar");
+        tag.setScope("request");
+        tag.doStartTag();
+        tag.doEndTag();
+        String output = ((MockHttpServletResponse) pageContext.getResponse()).getContentAsString();
+        assertEquals("", output);
+        assertEquals("/_ah/logout?continue=%2Fmy-destination-url", pageContext.getAttribute("myVar", PageContext.REQUEST_SCOPE));
+    }
+}
